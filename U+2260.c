2cc09a759d172baf75f8452e62a834cfdd3e0365
WodseZ <51055958+WodseZ@users.noreply.github.com>
2021-11-20 16:07:41 +0300

    Add files via upload

---
+++ U+2260.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int s1,s2;
+    printf("iki tamsayi giriniz\n");
+    printf("Bu iki sayinin karsilastirmasi yapilacaktir\n");
+    scanf("%d %d",&s1,&s2);
+    
+     if(s1==s2);
+    printf("%d esittir %d \n",s1,s2);
+    
+     if(s1!=s2);
+    printf("%d esit degildir %d \n",s1,s2);
+    
+     if(s1<s2);
+    printf("%d kucuktur %d \n",s1,s2);
+    
+     if(s1>s2);
+    printf("%d buyuktur %d \n",s1,s2);
+    
+     if(s1<=s2);
+    printf("%d kucuk veya esittir %d \n",s1,s2);
+    
+     if(s1>=s2);
+    printf("%d buyuk veya esittir %d \n",s1,s2);
+    
+    return 0;
+}
