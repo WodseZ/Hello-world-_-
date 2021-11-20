87eef445b636cf9c24e3887b0c8ec93b79df58bc
WodseZ <51055958+WodseZ@users.noreply.github.com>
2021-11-20 15:45:32 +0300

    Add files via upload

---
+++ +-.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+int main()
+{
+	
+	int sayi1,sayi2,toplam;
+	printf("ilk tamsayiyi giriniz\n");
+	scanf("%d",&sayi1);
+	printf("ikinci tamsayiyi giriniz\n");
+	scanf("%d",&sayi2);
+	toplam = sayi1+sayi2;
+	printf("toplam %d dir\n",toplam);
+	
+    int sayi3,sayi4,toplamm;
+	printf("ilk tamsayiyi giriniz\n");
+	scanf("%d",&sayi3);
+	printf("ikinci tamsayiyi giriniz\n");
+	scanf("%d",&sayi4);
+	toplamm = sayi3-sayi4;
+	printf("toplam %d dir\n",toplamm);
+	
+	int sayi5,sayi6,toplammm;
+	printf("ilk tamsayiyi giriniz\n");
+	scanf("%d",&sayi5);
+	printf("ikinci tamsayiyi giriniz\n");
+	scanf("%d",&sayi6);
+	toplammm = sayi5*sayi6;
+	printf("toplam %d dir\n",toplammm);
+	
+	int sayi7,sayi8,toplammmm;
+	printf("ilk tamsayiyi giriniz\n");
+	scanf("%d",&sayi7);
+	printf("ikinci tamsayiyi giriniz\n");
+	scanf("%d",&sayi8);
+	toplammmm = sayi7/sayi8;
+	printf("toplam %d dir\n",toplammmm);
+	return 0;
+}
